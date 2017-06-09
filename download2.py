678ca56baf217bb214019bece5767d6da8dbc9af
caiqiqi <77caikiki@gmail.com>
2017-06-09 18:14:25 +0800

    update

--- download2.py
+++ download2.py
@@ -4,7 +4,12 @@ from pyquery import PyQuery as pq
 
 class ButianDownloader():
     def __init__(self):
-        os.chdir('html')
+        DIR_PATH = 'html'
+        if not os.path.isdir(DIR_PATH):
+            print '[!] dir not exists'
+            print '[*] making dir...'
+            os.mkdir(DIR_PATH)
+        os.chdir(DIR_PATH)
         self.url = "https://loudong.360.cn/Loo/index/p/{0}.html"
         self.f = "{0}.html"
 
@@ -14,13 +19,18 @@ class ButianDownloader():
 
     def download(self, element):
         try:
-            for i in xrange(1, 4614):
+            for i in xrange(1, 4619):
                 u = self.url.format(i)
                 f = self.f.format(i)
                 ele = self.init_pq(u, element)
-                print "[*] " + u + "-> " + f
+                print "[*] " + u + " -> " + f
                 with open(f, 'w') as file:
-                    file.write(ele.text().encode('utf-8'))
+                    count = 30
+                    for j in ele.items(element):
+                        file.write(j.text().encode('utf-8') + '\n')
+                        count=count-1
+                        if count == 0:
+                            break
         except KeyboardInterrupt:
             pass
 
---
+++ test.py
@@ -0,0 +1,6 @@
+count = 5
+for i in xrange(1,10):
+    print i
+    count = count -1
+    if count == 0:
+        break
